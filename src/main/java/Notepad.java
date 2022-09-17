121f5d961a3c4ed3b4cd01ddf539de05261c6459
Anubhav Pandey <pandeyanubhav888@gmail.com>
2022-09-18 00:31:49 +0530

    add more functionality

--- src/main/java/Notepad.java
+++ src/main/java/Notepad.java
@@ -2,22 +2,23 @@ import com.formdev.flatlaf.FlatDarculaLaf;
 import com.formdev.flatlaf.FlatLightLaf;
 import com.formdev.flatlaf.IntelliJTheme;
 
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.UIManager;
-public class Notepad {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class Notepad implements ActionListener {
 
     UIManager.LookAndFeelInfo[] looks;
     String title = "Untitled - Notepad";
     JFrame jf;
     JMenuBar menubar;
     JMenu file;
-    JMenuItem neww, open , save , saveas , exit;
+    JMenuItem neww, open , save , saveas , exit , page_setup , prints;
     JTextArea text;
+    File filee;
     Notepad(){
         try{
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -32,21 +33,36 @@ public class Notepad {
         file = new JMenu("File");
 
         neww = new JMenuItem("New");
+        neww.addActionListener(this);
         file.add(neww);
 
         open = new JMenuItem("Open");
+        open.addActionListener(this);
         file.add(open);
 
         save = new JMenuItem("Save");
+        save.addActionListener(this);
         file.add(save);
 
         saveas = new JMenuItem("Save as");
+        saveas.addActionListener(this);
         file.add(saveas);
         // Added separator
         file.addSeparator();
 
+        page_setup = new JMenuItem("Page Setup");
+        page_setup.addActionListener(this);
+        file.add(page_setup);
+        prints = new JMenuItem("Print");
+        prints.addActionListener(this);
+        file.add(prints);
+
+        file.addSeparator();
+
         exit = new JMenuItem("Exit");
+        exit.addActionListener(this);
         file.add(exit);
+
         menubar.add(file);
         jf.setJMenuBar(menubar);
 
@@ -59,6 +75,42 @@ public class Notepad {
 
         jf.setVisible(true);
         //
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == neww){
+            newNotepad();
+        }
+        if(e.getSource() == exit){
+            System.exit(0);
+        }
+        if(e.getSource() == save){
+            setSave();
+        }
+    }
+    public void newNotepad(){
+        text.setText("");
+    }
+    public void setSave()  {
+        JFileChooser fileChooser = new JFileChooser();
+        int i = fileChooser.showSaveDialog(jf);
+        if(i!=0) JOptionPane.showMessageDialog(fileChooser,"File not Saved!" , "👋📁" , JOptionPane.WARNING_MESSAGE);
+        else {
+            try {
+                String data = text.getText();
+                byte[] b = data.getBytes();
+                filee = fileChooser.getSelectedFile();
+                FileOutputStream fos = new FileOutputStream(filee);
+                fos.write(b);
+                fos.close();
+                setTitle(filee.getName());
+            }
+            catch (Exception e) { e.printStackTrace();}
+        }
+    }
+    public void setTitle(String title){
+        jf.setTitle(title);
     }
 }
 
